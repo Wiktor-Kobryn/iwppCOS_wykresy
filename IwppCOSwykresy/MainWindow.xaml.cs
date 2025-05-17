c6c459ad8dc7087586dedf44f78df00159f8026f
KacpeRygal <92952265+KacpeRygal@users.noreply.github.com>
2025-05-17 19:58:17 +0200

    Revert "zdatne do publikacji"
    This reverts commit 4c2978742a7d0837740b4ab4edbd8ea977189fbf.

--- IwppCOSwykresy/MainWindow.xaml.cs
+++ IwppCOSwykresy/MainWindow.xaml.cs
@@ -280,7 +280,6 @@ public partial class MainWindow : Window
         }
 
         chartNormalizedData.Series = allSeries;
-        chartNormalizedData.XAxes = [new Axis { Name = "Ilość pomiarów" }];
     }
 
     private void btnHideZones_Click(object sender, RoutedEventArgs e)
@@ -382,8 +381,7 @@ public partial class MainWindow : Window
             {
                 using (var writer = new StreamWriter(saveFileDialog.FileName))
                 {
-                    writer.Write("Nr");
-                    writer.Write(";Czas [s]");
+                    writer.Write("Czas [s]");
                     for (int i = 0; i < seriesCountMax; i++)
                     {
                         if (isSeriesChosen[i])
@@ -392,10 +390,8 @@ public partial class MainWindow : Window
                     writer.WriteLine();
 
                     int length = rawData.time.Count;
-                    int nr = 0;
                     for (int i = dataToViewStart; i < length; i++)
                     {
-                        writer.Write(nr++ + ";");
                         writer.Write($"{rawData.time[i].ToString("0.###", CultureInfo.InvariantCulture)}");
                         for (int j = 0; j < seriesCountMax; j++)
                         {
